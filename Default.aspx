664a0a48c065e4f1f8454aa35a7d7ab41484e99f
Stefan <stefankarac12345@gmail.com>
2023-06-11 15:02:44 +0200

    KnjigaUtisaka

---
+++ Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="KnjigaUtisakaaA._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+
+
+    <asp:Label ID="Label1" runat="server" Text="Ime:"></asp:Label>
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+    <asp:Label ID="Label2" runat="server" Text="Email:"></asp:Label>
+    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label3" runat="server" Text="Komentar:"></asp:Label>
+    <asp:TextBox ID="TextBox3" runat="server" Height="148px" Width="158px"></asp:TextBox><br />
+    <asp:Button ID="Button1" runat="server" Text="Dodaj komentar" OnClick="Button1_Click" Width="132px" />
+    <br />
+
+
+<asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:KnjigaUtisakaSKConnectionString %>" SelectCommand="SELECT * FROM [Utisak]"></asp:SqlDataSource>
+
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+namespace KnjigaUtisakaaA
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            using (SqlConnection connection = new SqlConnection(
+               @"Data Source =.\SQLEXPRESS; Initial Catalog = KnjigaUtisakaSK; Integrated Security = True"))
+            {
+                connection.Open();
+                string InsertCommand = "INSERT INTO Utisak(Ime,Email,Komentar) values(@ime,@email,@komentar)";
+                using (SqlCommand command = new SqlCommand(InsertCommand, connection))
+                {
+                    
+                    command.Parameters.AddWithValue("@ime", TextBox1.Text);
+                    command.Parameters.AddWithValue("@email", TextBox2.Text);
+                    command.Parameters.AddWithValue("@komentar", TextBox3.Text);
+                    command.ExecuteNonQuery();
+                   
+                }
+                    
+            }
+        }
+    }
+}
